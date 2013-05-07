cc101a439f150e3779159a1d1d8a09d99aa04b7b
Rufino Mendoza <iMac_2009@Home>
2013-05-07 17:09:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsychicBear::Application.config.secret_token = '000e2b1115270fd2ae5cebc1fb0637430fb38305dc13816a278e98b7bedb41e9d2027a5672b0e86c3efd6023d2e20b5462368405ff82b3f1fb06455223b148ee'
